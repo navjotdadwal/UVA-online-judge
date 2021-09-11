910e287023bb126b5d8c1be83db0da59035e823d
navjotdadwal <59385365+navjotdadwal@users.noreply.github.com>
2021-09-11 17:09:15 +0530

    Create Sub-prime-statementnotclear.cpp

---
+++ Ad-hoc/Easy/Sub-prime-statementnotclear.cpp
@@ -0,0 +1,83 @@
+// https://onlinejudge.org/index.php?option=onlinejudge&Itemid=8&page=show_problem&problem=2726
+//check if after simulation all banks have ≥ 0 reserve
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>  
+#include <complex>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <list>
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <algorithm>
+#define trace1(x) cerr<<#x<<x<<endl
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <fstream>
+ 
+using namespace std;
+ 
+#define all(x) (x).begin(),(x).end()
+#define int long long int
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define F first
+#define S second
+#define inf 1e18
+#define vi vector<int>
+#define Point complex<int>
+#define X real()
+#define Y imag()
+#define pii pair<int,int>
+#define num0Beg(x) __builtin_clz(x) // no. of zero in beginning of bit representation
+#define num0end(x) __builtin_ctz(x) // no. of zero in the end
+#define numOf1(x) __builtin_popcount(x) // no. of ones in bit rep.
+#define parity(x) __builtin_parity(x) // parity of 1
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+
+int32_t main()
+{
+    FAST
+    int tt=1;
+    //cin >> tt;
+    while(tt--)
+    {         
+      int b,n;
+      while(1)
+      {
+        cin>>b>>n;
+        if(b==0 && n==0)
+          break;
+        int a[b+1];
+        for(int i=1;i<=b;i++)
+          cin>>a[i];
+        int fl=0;
+        for(int i=0;i<n;i++)
+        {
+          int x,y,z;
+          cin>>x>>y>>z;
+          a[x]-=z;
+          a[y]+=z;
+        }
+        for(int i=1;i<=b;i++)
+          if(a[i]<0)
+           fl=1;
+        if(fl)
+          cout<<"N\n";
+        else 
+          cout<<"S\n";   
+      }       
+    }
+    return 0;
+}
